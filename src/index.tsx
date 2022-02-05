192b1862057b467bb0ef8a6dd571237cd3568779
Guram <gurikakhasia@gmail.com>
2022-02-05 15:25:21 +0400

    Add transpile

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,53 @@
 import * as esbuild from "esbuild-wasm";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
     const [input, setInput] = useState("");
     const [code, setCode] = useState("");
+    const ref = useRef<any>();
 
     useEffect(() => {
         startService();
     }, []);
 
     const startService = async () => {
-        const service = await esbuild.startService({
+        ref.current = await esbuild.startService({
             worker: true,
             wasmURL: "/esbuild.wasm",
         });
-        console.log(service);
     };
 
-    const onClick = () => {
-        console.log(input);
+    const onClick = async () => {
+        if (!ref.current) {
+            return;
+        }
+
+        const result = await ref.current.transform(input, {
+            loader: "jsx",
+            target: "es2015",
+        });
+
+        setCode(result.code);
     };
     return (
         <div>
             <textarea
+                rows={10}
+                cols={100}
+                placeholder="Write some js code..."
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
             ></textarea>
             <div>
-                <button onClick={onClick}>Submit</button>
+                <button onClick={onClick}>Transpile</button>
             </div>
-            <pre>{code}</pre>
+            {code && (
+                <>
+                    <h1>ES2015</h1>
+                    <pre>{code}</pre>
+                </>
+            )}
         </div>
     );
 };
