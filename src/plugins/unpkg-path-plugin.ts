78df12f230ae52d5e5f5438b059087600dc0545c
Guram <gurikakhasia@gmail.com>
2022-02-06 21:49:50 +0400

    Refactor plugin

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,29 +1,29 @@
 import * as esbuild from "esbuild-wasm";
-import axios from "axios";
-import localForage from "localforage";
 
-const fileCache = localForage.createInstance({
-    name: "fileCache",
-});
 
 export const unpkgPathPlugin = () => {
     return {
         name: "unpkg-path-plugin",
         setup(build: esbuild.PluginBuild) {
+            //handle index.js
+            build.onResolve({ filter: /(^index\.js$)/ }, () => {
+                return { path: "index.js", namespace: "a" };
+            });
+
+            //handle relative path
+            build.onResolve({ filter: /^\.+\// }, (args: any) => {
+                return {
+                    namespace: "a",
+                    path: new URL(
+                        args.path,
+                        `https://unpkg.com${args.resolveDir}/`,
+                    ).href,
+                };
+            });
+            //handle nested path
             build.onResolve({ filter: /.*/ }, async (args: any) => {
-                console.log("onResolve", args);
                 if (args.path === "index.js") {
                     return { path: args.path, namespace: "a" };
-                }
-
-                if (args.path.includes("./") || args.path.includes("../")) {
-                    return {
-                        namespace: "a",
-                        path: new URL(
-                            args.path,
-                            `https://unpkg.com${args.resolveDir}/`,
-                        ).href,
-                    };
                 }
 
                 return {
@@ -32,35 +32,6 @@ export const unpkgPathPlugin = () => {
                 };
             });
 
-            build.onLoad({ filter: /.*/ }, async (args: any) => {
-                console.log("onLoad", args);
-
-                if (args.path === "index.js") {
-                    return {
-                        loader: "jsx",
-                        contents: `
-                            const reactSelect = require('react-select')
-                        `,
-                    };
-                }
-
-                const cachedResult =
-                    await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-
-                if (cachedResult) return cachedResult;
-
-                const { data, request } = await axios.get(args.path);
-
-                const result: esbuild.OnLoadResult = {
-                    loader: "jsx",
-                    contents: data,
-                    resolveDir: new URL("./", request.responseURL).pathname,
-                };
-
-                await fileCache.setItem(args.path, result);
-
-                return result;
-            });
         },
     };
 };
