85d308bff6d7e8571524c1920e50e9c9995439ef
Guram <gurikakhasia@gmail.com>
2022-03-03 23:21:19 +0400

    Add markdown editor

---
+++ src/components/text-editor.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect, useRef } from "react";
+import MDEditor from "@uiw/react-md-editor";
+import "./text-editor.css";
+
+const TextEditor: React.FC = () => {
+    const ref = useRef<HTMLDivElement | null>(null);
+    const [editing, setEditing] = useState(false);
+    const [value, setValue] = useState("# Header");
+
+    useEffect(() => {
+        const listener = (e: MouseEvent) => {
+            if (
+                ref.current &&
+                e.target &&
+                ref.current.contains(e.target as Node)
+            ) {
+                return;
+            }
+            setEditing(false);
+        };
+        document.addEventListener("click", listener, { capture: true });
+
+        return () => {
+            document.removeEventListener("click", listener, { capture: true });
+        };
+    }, []);
+
+    if (editing) {
+        return (
+            <div className="text-editor" ref={ref}>
+                <MDEditor value={value} onChange={(v) => setValue(v || "")} />
+            </div>
+        );
+    }
+    return (
+        <div className="text-editor" onClick={() => setEditing(true)}>
+            <MDEditor.Markdown source={value} />
+        </div>
+    );
+};
+
+export default TextEditor;
